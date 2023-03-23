b2b326908b120c466af754d9da3254da5546d4d9
PrinceH00 <hamz0230@stud.kea.dk>
2023-03-23 14:29:21 +0100

    Tiltøjet HEROID

--- src/main/java/com/example/superherov4/Models/Superhero.java
+++ src/main/java/com/example/superherov4/Models/Superhero.java
@@ -4,11 +4,13 @@ public class Superhero {
     String HeroHero;
     String PrivateName;
     int CreationYear;
+    private int HeroId;
 
-    public Superhero(String heroHero, String privateName, int creationYear) {
+    public Superhero(String heroHero, String privateName, int creationYear,int heroId) {
         HeroHero = heroHero;
         PrivateName = privateName;
         CreationYear = creationYear;
+        HeroId = heroId;
     }
 
     public String HeroHero() {
@@ -21,5 +23,29 @@ public class Superhero {
 
     public int CreationYear() {
         return CreationYear;
+    }
+
+    public int HeroId() {
+        return HeroId;
+    }
+
+    public Superhero setHeroHero(String heroHero) {
+        HeroHero = heroHero;
+        return this;
+    }
+
+    public Superhero setPrivateName(String privateName) {
+        PrivateName = privateName;
+        return this;
+    }
+
+    public Superhero setCreationYear(int creationYear) {
+        CreationYear = creationYear;
+        return this;
+    }
+
+    public Superhero setHeroId(int heroId) {
+        HeroId = heroId;
+        return this;
     }
 }
